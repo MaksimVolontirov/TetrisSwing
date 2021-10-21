b1fe925959a684e7e960062ce3599b720bcbcc29
MaksimVolontirov <maks.volontirov@gmail.com>
2021-10-21 23:53:08 +0300

    Second commit

--- src/com/company/TetrisSwing.java
+++ src/com/company/TetrisSwing.java
@@ -1,48 +1,73 @@
 package com.company;
 import javax.swing.*;
 import java.awt.*;
-//import java.awt.geom.Rectangle2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-final public class TetrisSwing {
-    static final int WINDOW_WIDTH = 380;
-    static final int WINDOW_HEIGHT = 440;
-    public static void main(String[] args) {
-        TetrisSwing.getFrame();
+
+public class TetrisSwing extends JComponent implements KeyListener, ActionListener {
+    static int x,y = 0;
+    Rectangle rectangle = new Rectangle(x+1,y+1,28,28);
+    Timer timer = new Timer(5,this);
+    static final int WINDOW_WIDTH = 570;
+    static final int WINDOW_HEIGHT = 660;
+        public static void main(String[] args) {
+            TetrisSwing main = new TetrisSwing();
+            JFrame frame = getFrame();
+            frame.add(main);
+            frame.addKeyListener(main);
+
     }
-    private static void getFrame(){
+    static JFrame getFrame(){
         JFrame frame = new JFrame();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        //frame.setResizable(false);
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension dimension = toolkit.getScreenSize();
-        frame.setBounds((dimension.width- WINDOW_WIDTH) / 2,(dimension.height - WINDOW_HEIGHT) / 2,WINDOW_WIDTH,WINDOW_HEIGHT);
+        frame.setBounds((dimension.width- WINDOW_WIDTH) / 2,(dimension.height - WINDOW_HEIGHT) / 2,WINDOW_WIDTH+14,WINDOW_HEIGHT+37);
         frame.setTitle("T E T R I S");
-        DrawPanel drawPanel = new DrawPanel();
+        return(frame);
+    }
+    public void paint(Graphics g){
+        Graphics2D g2d = (Graphics2D)g;
+        g.setColor(Color.BLACK);
+        g.fillRect(0,0,WINDOW_WIDTH, WINDOW_HEIGHT);
+        g.setColor(Color.DARK_GRAY);
+        g.fillRect(30,30,330,600);
+        g.setColor(Color.GRAY);
+        g.drawRect(30,30,330,600);
+        g2d.setColor(Color.WHITE);
+        g2d.fill(rectangle);
+        timer.start();
+    }
 
-    }
-    static class DrawPanel extends JPanel{
-        protected void paintComponent(Graphics g){
-            g.setColor(Color.BLACK);
-            g.fillRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
-            g.setColor(Color.WHITE);
-            g.fillRect(0,0,20,20);
-        }
-    }
-/*class RunApplication implements Runnable{
-    Graphics g;
-    void setGraphics(Graphics g){
-        this.g = g;
-    }
     @Override
-    public void run() {
-        final int SIZE = 20;
-        for (int i = 0; i < 10; i++){
-            g.drawRect(i*20,i*20,SIZE,SIZE);
-            try{
-                Thread.sleep(1000);
-            }catch(Exception exc){}
-        }
-    }*/
+    public void actionPerformed(ActionEvent e) {
+        repaint();
+    }
 
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_RIGHT){
+                x += 30;
+                rectangle.setLocation(x+1,y+1);
+        }
+        if(e.getKeyCode()==KeyEvent.VK_DOWN){
+                y += 30;
+                rectangle.setLocation(x+1,y+1);
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
