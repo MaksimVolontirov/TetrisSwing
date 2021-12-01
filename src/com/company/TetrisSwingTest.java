9f87bbde4624726719c49f8a3c73efc041ac016a
Ybyf25 <92331302+Ybyf25@users.noreply.github.com>
2021-12-02 01:32:12 +0300

    Update TetrisSwingTest.java

--- src/com/company/TetrisSwingTest.java
+++ src/com/company/TetrisSwingTest.java
@@ -4,8 +4,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class TetrisSwingTest {
     @Test
     @DisplayName("RestartGameTest")
@@ -14,5 +12,69 @@ class TetrisSwingTest {
         TetrisSwing.restartGame();
         boolean actualRestart = TetrisSwing.restart;
         Assertions.assertEquals(expectedRestart, actualRestart);
+    }
+
+    @Test
+    @DisplayName("RestartFieldTest")
+    void RestartFieldTest() {
+        byte[][] expected = new byte[25][12];
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 12; j++) {
+                expected[i][j] = 0;
+            }
+        }
+        TetrisSwing.restartGame();
+        byte[][] actual = TetrisSwing.field;
+        for (int i = 0; i < 25; i++) {
+            for (int j = 0; j < 12; j++) {
+                Assertions.assertEquals(expected[i][j], actual[i][j]);
+            }
+        }
+
+    }
+
+    @Test
+    @DisplayName("AmountTest")
+    void AmountTest() {
+        TetrisSwing.nextFigure = 4;
+        int expected = 1;
+        TetrisSwing.makeNextFigure();
+        int actual = TetrisSwing.amount;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("AmountTest2")
+    void AmountTest2() {
+        byte[] nextFigure = {1, 2, 3, 5, 6, 7};
+        for (int i = 0; i < 6; i++) {
+            TetrisSwing.nextFigure = nextFigure[i];
+        }
+        int expected = 4;
+        TetrisSwing.makeNextFigure();
+        int actual = TetrisSwing.amount;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("SpeedTest")
+    void SpeedTest() {
+        TetrisSwing.speed = 25;
+        TetrisSwing.score = 190;
+        double expected = 20;
+        TetrisSwing.removeString();
+        double actual = TetrisSwing.speed;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("SpeedTest2")
+    void SpeedTest2() {
+        TetrisSwing.speed = 10;
+        TetrisSwing.score = 120;
+        double expected = 10;
+        TetrisSwing.removeString();
+        double actual = TetrisSwing.speed;
+        Assertions.assertEquals(expected, actual);
     }
 }
